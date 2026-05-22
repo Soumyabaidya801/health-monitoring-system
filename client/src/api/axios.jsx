e66b1ab623a34e74cb9a8f520b0b102278c905d3
Soumyabaidya801 <detcsoumyabaidya@gmail.com>
2026-05-22 11:22:59 +0530

    update production api url

--- client/src/api/axios.jsx
+++ client/src/api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api/auth',
+  baseURL: 'https://health-monitoring-system-6hxx.onrender.com',
 });
 
 export default api;
